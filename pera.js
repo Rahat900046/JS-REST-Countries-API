39b84ac089f9d610bd910519273d9c0e82389088
Rahat <mdrahatulislam121@.com>
2022-05-19 13:08:34 +0600

    third comm2t

--- pera.js
+++ pera.js
@@ -17,7 +17,6 @@ function initialize(countriesData) {
     let options = "";
 
     countries.forEach(country => options += `<option value="${country.alpha3Code}">${country.name}</option>`);
-
     countriesList.innerHTML = options;
     countriesList.selectedIndex = Math.floor(Math.random() * countriesList.length);
     displayCountryInfo(countriesList[countriesList.selectedIndex].value);
@@ -30,7 +29,11 @@ function displayCountryInfo(countryByAlpha3Code) {
     document.getElementById("capital").innerHTML = countryData.capital;
     document.getElementById("dialing-code").innerHTML = `+${countryData.callingCodes[0]}`;
     document.getElementById("population").innerHTML = countryData.population.toLocaleString("en-US");
+    document.getElementById("area").innerHTML = countryData.area.toLocaleString("en-US");
     document.getElementById("currencies").innerHTML = countryData.currencies.filter(c => c.name).map(c => `${c.name} (${c.code})`).join(", ");
     document.getElementById("region").innerHTML = countryData.region;
     document.getElementById("subregion").innerHTML = countryData.subregion;
+    document.getElementById("nativename").innerHTML = countryData.nativeName;
+    document.getElementById("languages").innerHTML = countryData.languages[0].name;
+
 }
